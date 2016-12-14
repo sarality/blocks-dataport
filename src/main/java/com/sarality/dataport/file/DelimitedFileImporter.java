2d44a2cce9555f51fa5397233dbe29bc8e5f6a66
Abhideep Singh <abhi@dothat.in>
2016-12-14 21:06:47 +0530

    Import Status populated during Import Process

--- src/main/java/com/sarality/dataport/file/DelimitedFileImporter.java
+++ src/main/java/com/sarality/dataport/file/DelimitedFileImporter.java
@@ -1,13 +1,17 @@
 package com.sarality.dataport.file;
 
+import com.sarality.task.Task;
+import com.sarality.task.TaskProgressPublisher;
+
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Import Data provided by the tab or comma separated file.
  *
  * @author abhideep@ (Abhideep Singh)
  */
-public class DelimitedFileImporter {
+public class DelimitedFileImporter implements Task<Void, ImportStatus, ImportStatus> {
   private final DelimitedFileReader inputFileReader;
   private final FileLineProcessor lineProcessor;
   private final DelimitedErrorFileWriter errorFileWriter;
@@ -19,9 +23,30 @@ public class DelimitedFileImporter {
     this.errorFileWriter = errorFileWriter;
   }
 
-  public void importFile() throws IOException {
+  private int countLines() throws IOException {
     errorFileWriter.open();
-    inputFileReader.readAll(lineProcessor, errorFileWriter);
+    DelimitedFileLineCounter lineCounter = new DelimitedFileLineCounter();
+    inputFileReader.readAll(lineCounter, null, null);
+    return lineCounter.getTotalItemCount();
+  }
+
+  private ImportStatus importFile(int totalLInes, TaskProgressPublisher<ImportStatus> progressPublisher)
+      throws IOException {
+    errorFileWriter.open();
+    inputFileReader.getStatus().setTotalItemCount(totalLInes);
+    inputFileReader.readAll(lineProcessor, errorFileWriter, progressPublisher);
     errorFileWriter.close();
+    return inputFileReader.getStatus();
+  }
+
+  @Override
+  public ImportStatus execute(List<Void> inputList, TaskProgressPublisher<ImportStatus> progressPublisher) {
+    int numLines = 0;
+    try {
+      numLines = countLines();
+      return importFile(numLines, progressPublisher);
+    } catch (IOException e) {
+      return null;
+    }
   }
 }
