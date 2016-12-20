deb20f98f6dacaee5ae97168c00c98763d0ad139
Abhideep Singh <abhi@dothat.in>
2016-12-20 15:43:45 +0530

    Add Skip count for Empty lines that are skipped

--- src/main/java/com/sarality/dataport/file/importer/FileImportProgress.java
+++ src/main/java/com/sarality/dataport/file/importer/FileImportProgress.java
@@ -11,12 +11,15 @@ public class FileImportProgress {
   private int processedCount;
   private int successCount;
   private int errorCount;
+  private int skippedCount;
 
-  public FileImportProgress(int totalItemCount, int processedCount, int successCount, int errorCount) {
+  public FileImportProgress(int totalItemCount, int processedCount, int successCount, int errorCount,
+      int skippedCount) {
     this.totalItemCount = totalItemCount;
     this.processedCount = processedCount;
     this.successCount = successCount;
     this.errorCount = errorCount;
+    this.skippedCount = skippedCount;
   }
 
   public int getTotalItemCount() {
@@ -33,5 +36,9 @@ public class FileImportProgress {
 
   public int getErrorCount() {
     return errorCount;
+  }
+
+  public int getSkippedCount() {
+    return skippedCount;
   }
 }
