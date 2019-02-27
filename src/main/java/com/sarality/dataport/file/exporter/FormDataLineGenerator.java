0a183b5a73b3530155b214e63d87a232dac2c790
Satya <puniani@gmail.com>
2019-02-27 13:53:09 +0530

    add form data transforms to line generation

--- src/main/java/com/sarality/dataport/file/exporter/FormDataLineGenerator.java
+++ src/main/java/com/sarality/dataport/file/exporter/FormDataLineGenerator.java
@@ -5,6 +5,7 @@ import com.sarality.form.FormData;
 import com.sarality.form.FormDataConverter;
 import com.sarality.form.FormField;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,13 +17,25 @@ class FormDataLineGenerator<T> implements FileLineGenerator<T> {
 
   private final List<FormField> fieldList;
   private final FormDataConverter<T> dataConverter;
+  private final List<FormDataTransformer> transformerList = new ArrayList<>();
   private final Delimiter delimiter;
 
   FormDataLineGenerator(List<FormField> fieldList, FormDataConverter<T> dataConverter,
-      Delimiter delimiter) {
+      List<FormDataTransformer> transformerList, Delimiter delimiter) {
     this.fieldList = fieldList;
     this.dataConverter = dataConverter;
+    if (transformerList != null) {
+      this.transformerList.addAll(transformerList);
+    }
     this.delimiter = delimiter;
+  }
+
+  @Override
+  public void init() {
+    for (FormDataTransformer transformer : transformerList) {
+      transformer.init();
+      // TODO (@satya): add the form fields to the fields list
+    }
   }
 
   @Override
@@ -42,7 +55,7 @@ class FormDataLineGenerator<T> implements FileLineGenerator<T> {
 
   @Override
   public String generateLine(T data) {
-    FormData formData = dataConverter.generateFormData(data);
+    FormData formData = convert(data);
     StringBuilder builder = new StringBuilder();
     int ctr = 0;
     for (FormField field : fieldList) {
@@ -50,9 +63,21 @@ class FormDataLineGenerator<T> implements FileLineGenerator<T> {
       if (ctr > 0) {
         builder.append(delimiter.getStringValue());
       }
-      builder.append(value);
+      if (value != null) {
+        builder.append(value);
+      }
       ctr++;
     }
     return builder.toString();
   }
+
+  private FormData convert(T data) {
+    FormData formData = dataConverter.generateFormData(data);
+    // Any transformations to the column data
+    for (FormDataTransformer transformer : transformerList) {
+      transformer.transform(formData);
+    }
+    return formData;
+  }
+
 }
